3d0046ea31ec42a4708e32b707ea5672d5d12125
CortoBeck <corto@DESKTOP-B68SND7.>
2024-12-10 14:58:34 +0100

    rational

--- Rational.cpp
+++ Rational.cpp
@@ -6,11 +6,14 @@ private:
     int num;
     int denom;
 
+    friend Rational operator+(const Rational &r1, const Rational &r2);
+    friend bool operator==(const Rational &r1, const Rational &r2);
+
 public:
     Rational() : num(0), denom(1)
     {
     }
-
+   
     Rational(int n) : num(n), denom(1) {}
 
     Rational(int n, int d) : num(n), denom(d)
@@ -27,19 +30,7 @@ public:
         std::cout << num << "/" << denom << std::endl;
     }
 
-    // Opérateur global ==
-    friend bool operator==(const Rational &r1, const Rational &r2)
-    {
-        return (r1.num * r2.denom == r2.num * r1.denom);
-    }
-
-    // Opérateur global +
-    friend Rational operator+(const Rational &r1, const Rational &r2)
-    {
-        int new_num = r1.num * r2.denom + r2.num * r1.denom;
-        int new_denom = r1.denom * r2.denom;
-        return Rational(new_num, new_denom);
-    }
+    
 
 
     // Opérateur de conversion en float
@@ -48,6 +39,20 @@ public:
         return static_cast<float>(num) / static_cast<float>(denom);
     }
 };
+
+// Opérateur global +
+Rational operator+(const Rational &r1, const Rational &r2)
+{
+    int new_num = r1.num * r2.denom + r2.num * r1.denom;
+    int new_denom = r1.denom * r2.denom;
+    return Rational(new_num, new_denom);
+}
+
+// Opérateur global ==
+bool operator==(const Rational &r1, const Rational &r2)
+{
+    return (r1.num * r2.denom == r2.num * r1.denom);
+}
 
 int main()
 {
