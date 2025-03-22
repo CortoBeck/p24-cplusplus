b15b0b55ae18c0bcbecdc49c29295097c7ed9341
CortoBeck <corto.beck@etu.minesparis.psl.eu>
2025-03-22 15:02:12 +0100

    Update stack_tmpl.h

--- stack_tmpl.h
+++ stack_tmpl.h
@@ -3,11 +3,11 @@
 #include <iostream>
 #include <stdexcept>
 
-template <typename T, int SIZE> // Template sur le type et la taille
+template <typename T, int SIZE>
 class Stack {
 private:
-    T pile[SIZE]; // Tableau pour stocker les éléments
-    int length{}; // Nombre d'éléments dans la pile, initialement vide
+    T pile[SIZE];
+    int length{};
 
 public:
     void push(T e);
@@ -17,17 +17,15 @@ public:
     bool is_full() const;
     void print() const;
 
-    // Surcharge de l'opérateur <<
     template <typename U, int N>
     friend std::ostream& operator<<(std::ostream& os, const Stack<U, N>& pile);
 };
 
-// Implémentation des méthodes
 
 template <typename T, int SIZE>
 inline void Stack<T, SIZE>::push(T e) {
     if (is_full()) {
-        throw std::overflow_error("Erreur : on essaye d'empiler sur une pile pleine.");
+        throw std::overflow_error("Erreur :pile pleine.");
     }
     pile[length] = e;
     length += 1;
@@ -36,7 +34,7 @@ inline void Stack<T, SIZE>::push(T e) {
 template <typename T, int SIZE>
 inline T Stack<T, SIZE>::get_top() const {
     if (is_empty()) {
-        throw std::underflow_error("Erreur : on essaye de lire une pile vide.");
+        throw std::underflow_error("Erreur : pile vide.");
     }
     return pile[length - 1];
 }
@@ -44,7 +42,7 @@ inline T Stack<T, SIZE>::get_top() const {
 template <typename T, int SIZE>
 inline T Stack<T, SIZE>::pop() {
     if (is_empty()) {
-        throw std::underflow_error("Erreur : on essaye de dépiler une pile vide.");
+        throw std::underflow_error("Erreur : pile vide.");
     }
     T top = pile[length - 1];
     length -= 1;
@@ -64,7 +62,7 @@ inline bool Stack<T, SIZE>::is_full() const {
 template <typename T, int SIZE>
 inline void Stack<T, SIZE>::print() const {
     if (is_empty()) {
-        throw std::runtime_error("Erreur : affichage impossible d'une pile vide.");
+        throw std::runtime_error("Erreur : pile vide.");
     }
 
     std::cout << "[ ";
